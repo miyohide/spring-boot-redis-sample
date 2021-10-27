436f0f9dae9a91dec7a3a56a45651d3687750e2e
Hidenori Miyoshi <miyohide@gmail.com>
2021-10-27 17:49:03 +0900

    create initial project using Spring Initializr

---
+++ src/main/java/com/github/miyohide/springbootredissample/SpringBootRedisSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.springbootredissample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/miyohide/springbootredissample/SpringBootRedisSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.springbootredissample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
